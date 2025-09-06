7c9de1ea18b41f5c74a9ecea815f095fe4cf14ba
Hugo (Chun-Ho) Lin <hugo970217@gmail.com>
2025-09-06 15:59:13 -0700

    chore: add image configuration options to follow Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    unoptimized: true,
+    qualities: [25, 50, 75, 100],
+  },
 };
 
 export default nextConfig;
